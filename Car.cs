45f0da625ddecda4a1ab1d4d943ce0e18f3a965d
“Lydia <lydiajagdeo@gmail.com>
2022-08-22 14:10:59 -0400

    Classes exercise 1

---
+++ Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Classes
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Classes;
+
+Car myCar = new Car();
+
+
+myCar.Make = "Dodge";
+myCar.Model = "Nitro";
+myCar.Year = 2007;
+
+Console.WriteLine(myCar.Make);
+Console.WriteLine(myCar.Model);
+Console.WriteLine(myCar.Year);
